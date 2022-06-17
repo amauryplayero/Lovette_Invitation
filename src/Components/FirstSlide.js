947a787936d1e863e01b06556cfadc638690235c
Amaury <amaury.lopezdiaz@gmail.com>
2022-06-18 05:56:54 +0800

    test

--- src/Components/FirstSlide.js
+++ src/Components/FirstSlide.js
@@ -25,25 +25,16 @@ export default function FirstSlide() {
     {/* <div>FirstSlide</div> */}
     <div className="everythingContainer">
     <img src="https://i.imgur.com/rpdn1W8.jpg" className="logoImg"></img>
-        <div className="typewriter">
-             <p className="invitationText">i want u with me celebrating my 22nd year around the sun.it’s happening june 25, 2022 @ 9pm. this party is invite-only
-            and byob. let me know if you'd like to take anyone along.
+        <div>
+             <p className="invitationText">i want u with me celebrating my 22nd year around the sun.
+             it’s happening june 25, 2022 @ 9pm. this party is invite-only
+            and byob. let me know if you'd like to take anyone along
             </p>
         </div>
-
-        <div className="typewriter">
-             <p className="invitationText">around the sun.it’s happening june 25, 2022 @ 9pm. 
-        </p>
-        </div>
-
-        <div className="typewriter">
-             <p className="invitationText">this party is invite-only
-            and byob. let me know if you'd like to take anyone along.
-        </p>
-        </div>
-
     <button onClick={()=>goToSecondSlide()} className="whatUThink">tap me to learn more abt the theme.</button>
     </div>
+
+   
     </>
   )
 }
