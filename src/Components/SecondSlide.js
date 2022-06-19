fcbba5415230089a5df9f7aca3c780f9f30aede3
Amaury <amaury.lopezdiaz@gmail.com>
2022-06-19 10:51:25 +0800

    ready

--- src/Components/SecondSlide.js
+++ src/Components/SecondSlide.js
@@ -18,7 +18,7 @@ console.log(typeof(window.location.href))
       <>
     <div className="everythingContainer" id="section2">
     <img src="https://i.imgur.com/rpdn1W8.jpg" className="logoImg"></img>
-    <p className="invitationText"><a href="https://docs.google.com/document/d/1ul_bNbM_iGVDNwm1dXy1_T2HUDDw7J07zEvp9NCkVTc/edit?usp=sharing" className="link">sultry virgin ss22</a> OR <a href="https://www.shushutongstudio.com/collection/20ss/" className="link">shushu/tong ss20</a><br></br>
+    <p className="invitationText"><a href="https://docs.google.com/document/d/1ul_bNbM_iGVDNwm1dXy1_T2HUDDw7J07zEvp9NCkVTc/edit?usp=sharing" className="link" targe="_blank">sultry virgin ss22</a> OR <a href="https://www.shushutongstudio.com/collection/20ss/" className="link" targe="_blank">shushu/tong ss20</a><br></br>
       to best prep for this party, pick an outfit 
       from either collection and imitate it! </p>
 
